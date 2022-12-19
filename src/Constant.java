c6f1570248dcc41716df430262f9e06531b75112
yossefHanyBATSY <119070257+yossefHanyBATSY@users.noreply.github.com>
2022-12-19 09:49:48 +0200

    Add files via upload

---
+++ src/Constant.java
@@ -0,0 +1,15 @@
+
+import java.awt.Color;
+
+public class Constant {
+
+    public static final Color[] COLOR_STATES = new Color[]{new Color(68, 189, 50), new Color(255, 157, 34), new Color(194, 54, 22)};
+    public static final Color BACKGROUND_COLOR = new Color(113, 183, 255);
+    public static final Color BACKGROUND_COLOR_2 = new Color(103, 110, 133);
+    public static final Color TABLE_COLOR = new Color(150, 25, 253);
+    public static final Color TABLE_COLOR_Boundary = new Color(130, 0, 159);
+    public static final Color STICK_COLOR = new Color(255, 255, 255);
+
+    private Constant() {
+    }
+}
